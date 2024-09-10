be310183e584441821a2da36011f18f71f8b693f
markrobrigado <87180350+markrobrigado@users.noreply.github.com>
2024-09-10 13:59:34 +0800

    Add JobPosting model.

---
+++ JobBoard/Models/JobPosting.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobBoard.Models
+{
+    public class JobPosting
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Company { get; set; }
+        [Required]
+        public string Location { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public bool IsApproved { get; set; } = false;
+        [Required]
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; }
+    }
+}
