08b118464f77ceb5448ac1b9f34091601e581ddd
markrobrigado <87180350+markrobrigado@users.noreply.github.com>
2024-09-10 14:06:27 +0800

    Add JobPosting DbSet to context and scaffold migration.

--- JobBoard/Data/ApplicationDbContext.cs
+++ JobBoard/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using JobBoard.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace JobBoard.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<JobPosting> JobPostings { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
         {
             
