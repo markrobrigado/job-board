bbb021998207cdd25af6f3386ea0f7476cccfd18
markrobrigado <87180350+markrobrigado@users.noreply.github.com>
2024-09-11 17:02:13 +0800

    Add in-memory database for JobPostingRepositoryTests.

--- JobBoard.Tests/JobPostingRepositoryTests.cs
+++ JobBoard.Tests/JobPostingRepositoryTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using JobBoard.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,14 @@ namespace JobBoard.Tests
 {
     internal class JobPostingRepositoryTests
     {
+        private readonly DbContextOptions<ApplicationDbContext> _options;
+
+        public JobPostingRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase("JobPostingDb").Options;
+        }
+
+        private ApplicationDbContext CreateDbContext() => new ApplicationDbContext(_options);
+
     }
 }
