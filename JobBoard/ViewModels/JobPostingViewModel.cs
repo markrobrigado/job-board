c4e2696b7c6c82015d166e0565a436c5586a35f7
markrobrigado <87180350+markrobrigado@users.noreply.github.com>
2024-09-12 18:26:59 +0800

    Add JobPostingViewModel.

---
+++ JobBoard/ViewModels/JobPostingViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobBoard.ViewModels
+{
+    public class JobPostingViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Company { get; set; }
+        [Required]
+        public string Location { get; set; }
+    }
+}
